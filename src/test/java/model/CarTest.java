19693d238b284906bf691b93d364075fe1e32717
Giuseppe Matheus (Beppe) <giuseppemath@gmail.com>
2022-11-24 00:33:34 -0300

    (test) using extracting with containsSequence

--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -8,18 +8,21 @@ import static org.assertj.core.api.Assertions.*;
 public class CarTest {
 
     @Test
-    public void testCarJunit5() {
+    public void test_car_properties_junit5() {
         Car car = new Car("Onix", "2017");
+
         assertNotNull(car);
         assertEquals("Onix", car.getModel());
         assertEquals("2017", car.getYear());
     }
 
     @Test
-    public void testCarAssertJ() {
+    public void test_car_properties_assertJ() {
         Car car = new Car("Onix", "2017");
-        assertThat(car).isNotNull().extracting("year", "model").isNotNull().contains("2017",
-                "Onix");
+
+        assertThat(car).isNotNull().extracting(Car::getYear, Car::getModel).isNotNull()
+                .containsSequence("2017", "Onix");
+
     }
 
 }
